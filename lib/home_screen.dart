d236d142a0ce8f3e99a6a1031bcf0e0356c48038
rafiizdhr <rafiizdihar9@gmail.com>
2023-11-06 12:15:45 +0800

    Test Card Swipe di Home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -38,9 +43,8 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(
           height: tinggi,
           width: lebar,
-          child: Text("asd"),
+          child: Example(),
           decoration: BoxDecoration(
-
             gradient: LinearGradient(
               colors: [
                 Color(0xFF7512B2),
@@ -52,6 +56,52 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class Example extends StatelessWidget {
+  List<Color> kolor = [Colors.blue, Colors.green, Colors.yellow];
+
+  @override
+  Widget build(BuildContext context) {
+    List<Container> woy = List.generate(
+      10,
+      (index) => Container(
+        alignment: Alignment.center,
+        child: Text(index.toString()),
+        color: kolor[Random().nextInt(4)],
+      ),
+    );
+
+    return AppinioSwiper(
+      cardsCount: 10,
+      onSwiping: (AppinioSwiperDirection direction) {
+        print(direction.toString());
+      },
+      cardsBuilder: (BuildContext context, int index) {
+        return woy[index];
+      },
+      swipeOptions: AppinioSwipeOptions.symmetric(horizontal: true),
+      onSwipe: (index, direction) {
+        if (direction == AppinioSwiperDirection.right) {}
+      },
+      loop: true,
+    );
+  }
+}
+
+class Kartu extends StatelessWidget {
+  Color warna;
+
+  Kartu({super.key, required this.warna});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: const Text('1'),
+      color: warna,
     );
   }
 }
